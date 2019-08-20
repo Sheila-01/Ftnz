b71f502218c4abda1b5e18b64529eb0696c58732
Sheila-01 <53211792+Sheila-01@users.noreply.github.com>
2019-08-20 15:32:03 +0300

    Update qtn1.py

--- qtn1.py
+++ qtn1.py
@@ -1,8 +1,8 @@
 #write a python function that takes a list and returns a new list with unique elements of the first list. Dont use list methods
-
-def myList(*list):                                  #creating a function with multiple arguments
-    # list = [2,4,5,6,7,3,7,2,4,1,2]
-    newlist = set(myList)
-    return newlist
-myList(2,4,5,6,7,3,7,2,4,1,2)
-print(newlist)
+def myList(list):                                  #creating a function 
+    newlist = []
+    for i in list:
+        if i not in newlist:
+            i.append(newlist)
+    return i
+print(myList(2,6,7,8,90,23,45))
