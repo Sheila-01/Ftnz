74b3bc301f5866f7e5ea4113cdff5c59cf95da7e
Sheila-01 <53211792+Sheila-01@users.noreply.github.com>
2019-08-20 15:15:25 +0300

    Update qtn2.py

--- qtn2.py
+++ qtn2.py
@@ -1,9 +1,10 @@
 #writea a python program to print the even numbers from a given list. 
 
 def Even(*number):                                           #creating a function with multiple arguments
-    
+    num = []
     for i in number:                                                    #looping through the numbers
         while i % 2 == 0:                                    #if number when divided by 2 remainder = 0
+            num.append(i)
             return i
 
 print("The Even numbers are:", Even(3,5,67,89,90,12,32,44,11)) 
